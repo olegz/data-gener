4324b9d3f65cc19c9715c07f6908a5566cf991dc
Oleg Zhurakousky <oleg@suitcase.io>
2014-08-14 08:31:46 -0400

    initial commit

--- src/main/java/oz/DataGenerator.java
+++ src/main/java/oz/DataGenerator.java
@@ -56,6 +56,6 @@ public class DataGenerator {
 		}
 		
 		writer.close();
-		System.out.println("Written file: " + generatedFile + "; Size: " + fs.getFileStatus(generatedFile).getLen()/1024000000 + "Gb");
+		System.out.println("Written file: " + generatedFile + "; Size: " + ((float)fs.getFileStatus(generatedFile).getLen()/1024000000) + "Gb");
 	}
 }
